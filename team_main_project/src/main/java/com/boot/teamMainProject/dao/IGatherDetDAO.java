55223a168c3c020e693b505f304c06bc1bc15f4c
YounSeoYeon <97738757+YounSeoYeon@users.noreply.github.com>
2022-02-18 00:39:18 +0900

    Update IGatherDetDAO.java

--- team_main_project/src/main/java/com/boot/teamMainProject/dao/IGatherDetDAO.java
+++ team_main_project/src/main/java/com/boot/teamMainProject/dao/IGatherDetDAO.java
@@ -1,9 +1,11 @@
 package com.boot.teamMainProject.dao;
 
 import com.boot.teamMainProject.model.GatherDetVO;
+import com.boot.teamMainProject.model.MemberVO;
 
 public interface IGatherDetDAO {
 	String gatdetLoad(int gatDetNo);
 	GatherDetVO detailViewBoard(int gatDetNo);
 	void insertGatherDet(GatherDetVO gat);
+	MemberVO MoveinsertGather(String memNick);
 }
