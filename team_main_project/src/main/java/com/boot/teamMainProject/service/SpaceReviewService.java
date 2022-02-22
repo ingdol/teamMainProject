3447c35324d58935633d6acaa64f48f73d1abfbc
rhqudco <rhqudco1204@naver.com>
2022-02-22 23:08:55 +0900

    bug fix

--- team_main_project/src/main/java/com/boot/teamMainProject/service/SpaceReviewService.java
+++ team_main_project/src/main/java/com/boot/teamMainProject/service/SpaceReviewService.java
@@ -2,6 +2,8 @@ package com.boot.teamMainProject.service;
 
 import java.util.ArrayList;
 
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,7 @@ import com.boot.teamMainProject.model.SpaceReviewVO;
 @Service
 public class SpaceReviewService implements ISpaceReviewService{
 
-//    @Setter(onMethod_ = {@Autowired})
+    @Setter(onMethod_ = {@Autowired})
 
     @Qualifier("ISpaceReviewDAO")
     ISpaceReviewDAO dao;
---
+++ team_main_project/src/main/resources/static/js/bch/MapForDetailView.js
@@ -0,0 +1,41 @@
+$(document).ready(function (){
+    var Addr_val = $('#address').val();
+
+    // 도로명 주소를 좌표 값으로 변환(API)
+    naver.maps.Service.geocode({
+        query: Addr_val
+    }, function(status, response) {
+        if (status !== naver.maps.Service.Status.OK) {
+            return alert('Something wrong!');
+        }
+
+        var result = response.v2, // 검색 결과의 컨테이너
+            items = result.addresses; // 검색 결과의 배열
+
+        // 리턴 받은 좌표 값을 변수에 저장
+        let x = parseFloat(items[0].x);
+        let y = parseFloat(items[0].y);
+
+        // 지도 생성
+        var map = new naver.maps.Map('map', {
+            center: new naver.maps.LatLng(y, x), // 지도를 열 좌표
+            zoom: 18
+        });
+
+        // 지도에 해당 좌표 마커(아이콘 설정)
+        var markerOptions = {
+            position: new naver.maps.LatLng(y, x), //마커찍을 좌표
+            map: map,
+            icon: {
+                url: 'resources/img/marker.png', //아이콘 경로
+                size: new naver.maps.Size(22, 36), //아이콘 크기
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35)
+            }
+        };
+
+        // 마커 생성
+        var marker = new naver.maps.Marker(markerOptions);
+
+    });
+});
