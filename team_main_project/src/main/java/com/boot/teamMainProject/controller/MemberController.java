3358ee54ad12d5c14bb1fe1f660cc8535e46d63f
ParkDohun <bee6438379@gmail.com>
2022-03-27 00:33:01 +0900

    마이페이지 내 게시글 css 및 출력 수정

--- team_main_project/src/main/java/com/boot/teamMainProject/controller/MemberController.java
+++ team_main_project/src/main/java/com/boot/teamMainProject/controller/MemberController.java
@@ -21,8 +21,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.boot.teamMainProject.model.CommunityVO;
+import com.boot.teamMainProject.model.GatheringVO;
 import com.boot.teamMainProject.model.MemberVO;
 import com.boot.teamMainProject.service.CommunityService;
+import com.boot.teamMainProject.service.GatheringService;
 import com.boot.teamMainProject.service.MemberService;
 
 @Controller
@@ -30,25 +32,27 @@ public class MemberController {
 
 	@Autowired
 	MemberService service;
-	
+	@Autowired
+	GatheringService Gatherservice;
+
 	@Autowired
 	CommunityService commuser;
 
 	@Autowired
 	SpaceReservationService reservationService;
-	
-	@RequestMapping(value = "/login", method = {RequestMethod.GET})
+
+	@RequestMapping(value = "/login", method = { RequestMethod.GET })
 	public String login() {
 
 		return "pdh/login";
 	}
-	
-	@RequestMapping(value = "/signup", method = {RequestMethod.GET})
+
+	@RequestMapping(value = "/signup", method = { RequestMethod.GET })
 	public String signup() {
 
 		return "pdh/signup";
 	}
-	
+
 	// 로그인
 	@ResponseBody
 	@RequestMapping(value = "/signin")
@@ -57,10 +61,10 @@ public class MemberController {
 		String checkVar = "fail";
 		MemberVO vo = service.selectNick(param.get("id"));
 		MemberVO resultChk = service.signIn(param);
-		
+
 		HttpSession session = request.getSession();
-		
-		if(resultChk != null) {
+
+		if (resultChk != null) {
 			session.setAttribute("sid", resultChk.getMemId());
 			session.setAttribute("snick", vo.getMemNick());
 			session.setAttribute("spoint", service.selectPoint(param.get("id")));
@@ -70,51 +74,51 @@ public class MemberController {
 
 		return checkVar;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/idcheck")
 	public HashMap<String, String> memIdCheck(@RequestBody String param) {
-		
+
 		String checkId = "use";
 		String memIdCheck = service.memIdCheck(param);
-		
-		if(memIdCheck == null) {
+
+		if (memIdCheck == null) {
 			checkId = "no_use";
 		}
-		
+
 		HashMap<String, String> id = new HashMap<String, String>();
 		id.put("id", checkId);
 		return id;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/nickcheck")
 	public HashMap<String, String> memNickCheck(@RequestBody String param) {
-		
+
 		String checkNick = "use";
 		String memNickCheck = service.memNickCheck(param);
-		
-		if(memNickCheck == null) {
+
+		if (memNickCheck == null) {
 			checkNick = "no_use";
 		}
-		
+
 		HashMap<String, String> nick = new HashMap<String, String>();
 		nick.put("nick", checkNick);
 		return nick;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/user-signup")
 	public String userSignup(@RequestBody HashMap<String, String> params) throws ParseException {
-		
+
 		String[] hobbySplit = params.get("hobby").split(",");
 		HashMap<String, String> hobbyArr = new HashMap<String, String>();
 		hobbyArr.put("hobby1", hobbySplit[0]);
 		hobbyArr.put("hobby2", hobbySplit[1]);
 		hobbyArr.put("hobby3", hobbySplit[2]);
-		
+
 		MemberVO vo = new MemberVO();
-		
+
 		vo.setMemName(params.get("name"));
 		vo.setMemId(params.get("email"));
 		vo.setMemPw(params.get("pw"));
@@ -127,10 +131,10 @@ public class MemberController {
 		vo.setMemArea(params.get("address"));
 		vo.setMemPoint(params.get("point"));
 		service.userSignup(vo);
-		
+
 		return "test";
 	}
-	
+
 	// 로그아웃
 	@RequestMapping(value = "/logout")
 	public String logout(HttpServletRequest request) {
@@ -139,122 +143,139 @@ public class MemberController {
 
 		return "redirect:/";
 	}
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 
-    @RequestMapping(value = "/start", method = RequestMethod.POST, consumes = "application/json")
-    @ResponseBody
-    public String startApp(@RequestBody String body) {
-        System.out.println(body);
-        logger.info(body);
-        return "/";
-    }
-    
-    @ResponseBody
-    @RequestMapping(value = "/auth-send")
-    public HashMap<String, String> authSend(@RequestBody String email, Locale locale, Model model){
-        JSONObject cred = new JSONObject();
-        JSONObject auth = new JSONObject();
-        JSONObject parent = new JSONObject();
+	@RequestMapping(value = "/start", method = RequestMethod.POST, consumes = "application/json")
+	@ResponseBody
+	public String startApp(@RequestBody String body) {
+		System.out.println(body);
+		logger.info(body);
+		return "/";
+	}
 
-        cred.put("username","name");
-        cred.put("password", "pw");
-        auth.put("tenantName", email);
-        auth.put("passwordCredentials", cred);
-        parent.put("auth", auth);
+	@ResponseBody
+	@RequestMapping(value = "/auth-send")
+	public HashMap<String, String> authSend(@RequestBody String email, Locale locale, Model model) {
+		JSONObject cred = new JSONObject();
+		JSONObject auth = new JSONObject();
+		JSONObject parent = new JSONObject();
 
-        URLConn conn = new URLConn("http://127.0.0.1",1516);
-        conn.urlPost(parent);
+		cred.put("username", "name");
+		cred.put("password", "pw");
+		auth.put("tenantName", email);
+		auth.put("passwordCredentials", cred);
+		parent.put("auth", auth);
 
-        System.out.println(MemberVO.getAuthNum());
-        
-        HashMap<String, String> num = new HashMap<String, String>();
-        num.put("num", MemberVO.getAuthNum());
+		URLConn conn = new URLConn("http://127.0.0.1", 1516);
+		conn.urlPost(parent);
 
-        return num;
-    }
-    
-    // 마이페이지
-    @RequestMapping(value = "/mypage")
-    public String myPage(Model model, HttpServletRequest request) {
-    	MemberVO vo = new MemberVO();
-    	HttpSession session = request.getSession();
-    	String id = (String) session.getAttribute("sid");
+		System.out.println(MemberVO.getAuthNum());
+
+		HashMap<String, String> num = new HashMap<String, String>();
+		num.put("num", MemberVO.getAuthNum());
+
+		return num;
+	}
+
+	// 마이페이지
+	@RequestMapping(value = "/mypage")
+	public String myPage(Model model, HttpServletRequest request) {
+		MemberVO vo = new MemberVO();
+		HttpSession session = request.getSession();
+		String id = (String) session.getAttribute("sid");
 		String snick = (String) session.getAttribute("snick");
+		
+		//세션에 id가 없을 경우 메인페이지로 리다이렉트
+		if(id == null || StringUtils.isEmpty(id)) {
+			return "redirect:/";
+		}
+		
 		ArrayList<SpaceReservationVO> ReservationList = reservationService.LookUpReservation(snick); // 내가 예약한 공간 조회
 		model.addAttribute("ReservationList", ReservationList);
 
-    	vo.setMemId(id);
-		model.addAttribute("join", service.gather(vo));
-    	
-    	return "pdh/mypage";
-    }
-    
-    @RequestMapping(value = "/mypage-gather")
-    public String myPageGather(Model model, HttpServletRequest request) {
-    	HttpSession session = request.getSession();
-    	String nick = String.valueOf(session.getAttribute("snick"));
-    	ArrayList<CommunityVO> nickCheck = commuser.memNickCheck(nick);
-    	
-    	model.addAttribute("nickCheck", nickCheck);
-    	return "pdh/mypage_gather";
-    }
-    
-    
-    // 정보 수정-로그인
-    @RequestMapping(value = "/change-info-signin")
-    public String changeInfoSignin(Model model, HttpServletRequest request) {
-    	HttpSession session = request.getSession();
-    	String sid = String.valueOf(session.getAttribute("sid"));
-    	
-    	model.addAttribute("sid", sid);
-    	
-    	return "pdh/mypage_change_info_signin";
-    }
-    
-    // 정보 수정
-    @RequestMapping(value = "/change-info-signup")
-    public String changeInfo(Model model, HttpServletRequest request) {
-    	HttpSession session = request.getSession();
-    	String id = String.valueOf(session.getAttribute("sid"));
-    	
-    	MemberVO mem = service.detailViewMember(id);
-    	
-    	if(StringUtils.isNotEmpty(mem.getMemBirth())) {
-    		String memBirth[] = mem.getMemBirth().split("-");
-    		if(memBirth.length > 2) {
-    			mem.setMemBirth1(memBirth[0]);
-    			mem.setMemBirth2(memBirth[1]);
-    			mem.setMemBirth3(memBirth[2]);
-    		}
-    	}
-    	
-    	model.addAttribute("mem", mem);
-    	
-    	return "pdh/mypage_change_info_signup";
-    }
-    
+		vo.setMemId(id);
+		
+		/** 로그인한 회원 정보로 가입한 모임 테이블 리스트 조회 */
+		MemberVO gather = new MemberVO();
+		gather = service.gather(vo);
+		List<GatheringVO> gatVoList = new ArrayList<GatheringVO>();
+		if(gather != null) {
+			gatVoList = Gatherservice.detailViewGatNoList(gather);
+		}
+		/** End 로그인한 회원 정보로 가입한 모임 테이블 리스트 조회 */
+
+		model.addAttribute("join", gather);
+		model.addAttribute("gatVoList", gatVoList);
+		model.addAttribute("userId", id);
+
+		return "pdh/mypage";
+	}
+
+	@RequestMapping(value = "/mypage-gather")
+	public String myPageGather(Model model, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		String nick = String.valueOf(session.getAttribute("snick"));
+		ArrayList<CommunityVO> nickCheck = commuser.memNickCheck(nick);
+
+		model.addAttribute("nickCheck", nickCheck);
+		return "pdh/mypage_gather";
+	}
+
+	// 정보 수정-로그인
+	@RequestMapping(value = "/change-info-signin")
+	public String changeInfoSignin(Model model, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		String sid = String.valueOf(session.getAttribute("sid"));
+
+		model.addAttribute("sid", sid);
+
+		return "pdh/mypage_change_info_signin";
+	}
+
+	// 정보 수정
+	@RequestMapping(value = "/change-info-signup")
+	public String changeInfo(Model model, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		String id = String.valueOf(session.getAttribute("sid"));
+
+		MemberVO mem = service.detailViewMember(id);
+
+		if (StringUtils.isNotEmpty(mem.getMemBirth())) {
+			String memBirth[] = mem.getMemBirth().split("-");
+			if (memBirth.length > 2) {
+				mem.setMemBirth1(memBirth[0]);
+				mem.setMemBirth2(memBirth[1]);
+				mem.setMemBirth3(memBirth[2]);
+			}
+		}
+
+		model.addAttribute("mem", mem);
+
+		return "pdh/mypage_change_info_signup";
+	}
+
 	@ResponseBody
 	@RequestMapping(value = "/user-change-info")
 	public void userChangeInfo(@RequestBody HashMap<String, String> params, HttpServletRequest request) {
-			
-			String[] hobbySplit = params.get("hobby").split(",");
-			HashMap<String, String> hobbyArr = new HashMap<String, String>();
-			hobbyArr.put("hobby1", hobbySplit[0]);
-			hobbyArr.put("hobby2", hobbySplit[1]);
-			hobbyArr.put("hobby3", hobbySplit[2]);
-			
-			MemberVO vo = new MemberVO();
-			
-			vo.setMemId(params.get("email"));
-			vo.setMemPw(params.get("pw"));
-			vo.setMemArea(params.get("address"));
-			vo.setMemPhone(params.get("phone"));
-			vo.setHobbyNo1(hobbyArr.get("hobby1"));
-			vo.setHobbyNo2(hobbyArr.get("hobby2"));
-			vo.setHobbyNo3(hobbyArr.get("hobby3"));
-			
-			service.userChangeInfo(vo);
+
+		String[] hobbySplit = params.get("hobby").split(",");
+		HashMap<String, String> hobbyArr = new HashMap<String, String>();
+		hobbyArr.put("hobby1", hobbySplit[0]);
+		hobbyArr.put("hobby2", hobbySplit[1]);
+		hobbyArr.put("hobby3", hobbySplit[2]);
+
+		MemberVO vo = new MemberVO();
+
+		vo.setMemId(params.get("email"));
+		vo.setMemPw(params.get("pw"));
+		vo.setMemArea(params.get("address"));
+		vo.setMemPhone(params.get("phone"));
+		vo.setHobbyNo1(hobbyArr.get("hobby1"));
+		vo.setHobbyNo2(hobbyArr.get("hobby2"));
+		vo.setHobbyNo3(hobbyArr.get("hobby3"));
+
+		service.userChangeInfo(vo);
 	}
 
 	@RequestMapping("ReservationList")
